45e0e8bf1ea3a5cdeda032885b4731298d4c0b10
Christopher Galloway <christopher.galloway@posteo.net>
2026-02-03 14:42:49 +0100

    Add update functionality to app

--- src/app.py
+++ src/app.py
@@ -13,17 +13,22 @@ if loan_selection == "Loan a book":
     book_df = st.dataframe(display_books(), selection_mode="single-row", on_select="rerun")
     if len(book_df.selection["rows"]) > 0:
         book = read_books().loc[book_df.selection["rows"][0]]
-
+        val1 = validate_loan_item(book)
+        if val1:
+            st.warning(val1)
     friend_df = st.dataframe(display_friends(), selection_mode="single-row", on_select="rerun")
     if len(friend_df.selection["rows"]) > 0:
         friend = read_friends().loc[friend_df.selection["rows"][0]]
-    try:
-        val1 = validate_loan_taker(friend)
-        val2 = validate_loan_item(book)
-        if val1:
-            st.warning(val1)
+        val2 = validate_loan_taker(friend)
         if val2:
             st.warning(val2)
+    try:
+        # val1 = validate_loan_taker(friend)
+        # val2 = validate_loan_item(book)
+        # if val1:
+        #     st.warning(val1)
+        # if val2:
+        #     st.warning(val2)
         if (not val1) & (not val2):
             loan_date = st.date_input("Loan date", key="loan_date")
             next_contact = st.date_input("Next check date", value=pd.Timestamp.today().date() + pd.Timedelta(30, "d"), key="loan_next_contact")
@@ -59,24 +64,24 @@ st.header("Books")
 book_selection = st.radio("What would you like to do?", options=["Review books", "Add a book", "Update a book"], index=None)
 if book_selection == "Add a book":
     title = st.text_input("Title:", key="create_title")
-    # if title:
-    #     val1 = validate_title(title)
-    #     if val1:
-    #         st.warning(val1)
+    if title:
+        val1 = validate_title(title)
+        if val1:
+            st.warning(val1)
     author = st.text_input("Author:", key="create_author")
     genre = st.text_input("Genre:", key="create_genre")
     isbn = st.text_input("ISBN:", key="create_isbn")
-    # if isbn:
-    #     val2 = validate_isbn(isbn)
-    #     if val2:
-    #         st.warning(val2)
-    try:
-        val1 = validate_title(title)
+    if isbn:
         val2 = validate_isbn(isbn)
-        if val1 and title:
-            st.warning(val1)
-        if val2 and isbn:
+        if val2:
             st.warning(val2)
+    try:
+        # val1 = validate_title(title)
+        # val2 = validate_isbn(isbn)
+        # if val1 and title:
+        #     st.warning(val1)
+        # if val2 and isbn:
+        #     st.warning(val2)
         if (not val1) & (not val2):
             if st.button("Submit", key="create_book"):
                 st.success(create_book(title, isbn, author, genre))
