d663f7279bb9aef37c519dcca94a84fca00b3296
Christopher Galloway <christopher.galloway@posteo.net>
2026-02-12 11:40:49 +0100

    Add con_EXAMPLE

---
+++ con_EXAMPLE.py
@@ -0,0 +1,7 @@
+schema = "sample_library"
+host = "127.0.0.1"
+user = "root"
+password = "YOUR PASSWORD"
+port = 3306
+
+connection_string = f'mysql+pymysql://{user}:{password}@{host}:{port}/{schema}'
