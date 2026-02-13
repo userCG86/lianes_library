13d87569905e3db7fffa87b8133bb3e98390728a
Christopher Galloway <christopher.galloway@posteo.net>
2026-02-13 13:34:09 +0100

    Add instruction comment

--- con_EXAMPLE.py
+++ con_EXAMPLE.py
@@ -1,7 +1,7 @@
 schema = "sample_library"
 host = "127.0.0.1"
 user = "root"
-password = "YOUR PASSWORD"
+password = "YOUR PASSWORD" # enter your password for local MySQL server INSIDE the quotes
 port = 3306
 
 connection_string = f'mysql+pymysql://{user}:{password}@{host}:{port}/{schema}'
